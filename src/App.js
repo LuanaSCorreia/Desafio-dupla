21cff361274236f6c0019c5c9eb7e94a03ed0a67
LuanaSCorreia <luanacorreia797@gmail.com>
2022-09-25 22:12:31 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import background from "./img/background.png"
+import styled from "styled-components";
+
+const Body = styled.body`
+background-image: ${background};
+
+` 
+function App() {
+  return (
+    <Body>
+       
+    </Body>
+  );
+}
+
+export default App;
